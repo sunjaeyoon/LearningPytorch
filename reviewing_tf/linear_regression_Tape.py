fd0d3185e0915f2d0ee32016a162719ecda55d8b
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2021-11-12 13:21:07 -0500

    small modifications

--- reviewing_tf/linear_regression_Tape.py
+++ reviewing_tf/linear_regression_Tape.py
@@ -2,14 +2,15 @@
 import numpy as np
 import tensorflow as tf
 
-w = tf.Variable(1,dtype=tf.float64)
-b = tf.Variable(1,dtype=tf.float64)
+def generate_dataset():
+    x_batch = np.linspace(0, 2, 100)
+    y_batch = 1.5 * x_batch + 0.5
+    return x_batch, y_batch
+
 
 def linear_model(x):
     return b + tf.multiply(x,w)
 
-optimizer = tf.keras.optimizers.Adam()
-loss_object = tf.keras.losses.MeanSquaredLogarithmicError()
 
 def train_step(x, y):
     with tf.GradientTape() as tape:
@@ -19,11 +20,19 @@ def train_step(x, y):
         grads = tape.gradient(loss_value, [b,w])
         optimizer.apply_gradients(zip(grads, [b,w]))
 
+x, y = generate_dataset()
 def train(epochs):
     for epoch in range(epochs):
             train_step(x, y)
     print ('Epoch {} finished'.format(epoch))
 
-train(epochs = 1000)
 
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.MeanSquaredLogarithmicError()
+w = tf.Variable(1,dtype=tf.float64)
+b = tf.Variable(1,dtype=tf.float64)
+
+
+train(epochs = 1000)
 print(w, b)
