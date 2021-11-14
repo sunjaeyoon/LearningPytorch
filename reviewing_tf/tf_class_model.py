bb4595cb319795f04f5d1380eb0bb7b5ae91bb9b
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2021-11-14 11:44:21 -0500

    tf custom Model file

---
+++ reviewing_tf/tf_class_model.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+
+class Linear(tf.keras.Model):
+  def __init__(self):
+    super(Linear, self).__init__()
+    self.W = tf.Variable(5., name='weight')
+    self.B = tf.Variable(10., name='bias')
+  def call(self, inputs):
+    return inputs * self.W + self.B
+
+
+# A toy dataset of points around 3 * x + 2
+NUM_EXAMPLES = 2000
+training_inputs = tf.random.normal([NUM_EXAMPLES])
+noise = tf.random.normal([NUM_EXAMPLES])
+training_outputs = training_inputs * 3 + 2 + noise
+
+# The loss function to be optimized
+def loss(model, inputs, targets):
+  error = model(inputs) - targets
+  return tf.reduce_mean(tf.square(error))
+
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets)
+  return tape.gradient(loss_value, [model.W, model.B])
+
+
+
+model = Linear()
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+
+print("Initial loss: {:.3f}".format(loss(model, training_inputs, training_outputs)))
+
+steps = 300
+for i in range(steps):
+  grads = grad(model, training_inputs, training_outputs)
+  optimizer.apply_gradients(zip(grads, [model.W, model.B]))
+  if i % 20 == 0:
+    print("Loss at step {:03d}: {:.3f}".format(i, loss(model, training_inputs, training_outputs)))
+
+print(model.W.numpy(), model.B.numpy())
